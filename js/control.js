47de5a21635cfb219a6dafa7fad7149647502437
Tu2201 <buithanhtu22012000@gmail.com>
2025-05-30 18:29:15 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,12 @@
+// JavaScript Documentv
+            var typed = new Typed('.element', {
+               	strings: ["Graphic Designer.", "Visual Merchandiser.","Apps Designer."],
+       	 	typeSpeed:50,
+        		backSpeed:50,
+        		loop:true,
+        		});
+$('.timeline-panel').click(function() {
+    $('.timeline-body', this).toggle(); // p00f
+});
+$('.counter').countUp();
+  AOS.init();
